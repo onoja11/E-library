0385e3173ee945388dde0e855b02f9f79d831e05
unknown <ochigbogodswill868@gmail.com>
2025-02-09 19:59:28 -0800

    new changes

--- api/index.php
+++ api/index.php
@@ -1,13 +1,19 @@
 <?php
 
-// Load the Composer autoloader
+// Define the path to the public directory
+define('LARAVEL_PUBLIC_PATH', __DIR__.'/../public');
+
+// Include the Composer autoloader
 require_once __DIR__ . '/../vendor/autoload.php';
+
+// Include Laravel’s bootstrap process (public/index.php)
+require_once LARAVEL_PUBLIC_PATH . '/index.php';
 
 // Get the Laravel application instance
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 // Use the HTTP kernel to handle the incoming request
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 // Capture the request and handle it
 $request = Illuminate\Http\Request::capture();
